17fa3614997e1706fb0b16ff4371b0341c4ce5a6
pbrcic14 <pbrcic@localhost.localdomain>
2021-10-27 09:03:42 +0200

    Add Vjezbe_3 task 3 (bad random)

--- Vjezbe_3/ElementaryParticle.cpp
+++ Vjezbe_3/ElementaryParticle.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <cmath>
 #include <cstdlib>
-#include <time.h>
+#include <ctime>
 #include <random>
 #include <ElementaryParticle.h>
 
@@ -36,6 +37,7 @@ void ElementaryParticle::setMomentum(double p1, double p2, double p3)
 	momentum[2] = p2;
 	momentum[3] = p3;
 	momentum[0] = sqrt(p1*p1 + p2*p2 + p3*p3 + mass*mass);
+	
 	cout << "Energy = " << momentum[0] << "\n";
 	cout << "Transversal momentum = " << sqrt(p1*p1 + p2*p2) << "\n";
 }
@@ -44,7 +46,6 @@ void ElementaryParticle::bosonDecay(ElementaryParticle* particle1, ElementaryPar
 	srand(time(NULL));
 	double r, E1, E2;
 	
-	// if decay is possible - random to decide channel, assign mass
 	if(isBoson == 0)
 		cout << "Decay is not possible.\n";
 	else
@@ -52,30 +53,29 @@ void ElementaryParticle::bosonDecay(ElementaryParticle* particle1, ElementaryPar
 		r = (double) rand() / RAND_MAX;
 		if(r < 0.214)
 		{
-			cout << "Decay to W bosons.\n";
+			cout << "Decay to W bosons.\n\n";
 			particle1 -> mass = 80.379;
 			particle2 -> mass = 80.379;
 		}
 		else if(r < 0.278)
 		{
-			cout << "Decay to tau leptons.\n";
+			cout << "Decay to tau leptons.\n\n";
 			particle1 -> mass = 1.777;
 			particle2 -> mass = 1.777;
 		}
 		else if(r < 0.304)
 		{
-			cout << "Decay to Z bosons.\n";
+			cout << "Decay to Z bosons.\n\n";
 			particle1 -> mass = 91.188;
 			particle2 -> mass = 91.188;
 		}
 		else
 		{
-			cout << "Decay to b quarks.\n";
+			cout << "Decay to b quarks.\n\n";
 			particle1 -> mass = 4.18;
 			particle2 -> mass = 4.18;
 		}
 		
-		// calculate energy
 		E1 = pow(particle1 -> mass, 2);
 		E2 = pow(particle2 -> mass, 2);
 		for(int i = 1; i < 4; i++)
@@ -88,13 +88,5 @@ void ElementaryParticle::bosonDecay(ElementaryParticle* particle1, ElementaryPar
 		}
 		particle1 -> momentum[0] = sqrt(E1);
 		particle2 -> momentum[0] = sqrt(E2);
-		
-		// output momentum 4-vector
-		cout << "Momentum 4-vector of particle 1:\n";
-		for(int i = 0; i < 4; i++)
-			cout << particle1 -> momentum[i] << "\n";
-		cout << "Momentum 4-vector of particle 2:\n";
-		for(int i = 0; i < 4; i++)
-			cout << particle2 -> momentum[i] << "\n";
 	}
 }
--- Vjezbe_3/analyzer.cpp
+++ Vjezbe_3/analyzer.cpp
@@ -1,35 +1,47 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 #include <ElementaryParticle.h>
 
 using namespace std;
 
 int main()
 {
+	double p1, p2, p3;	
+	ofstream file ("Analysis.txt");
+	
 	ElementaryParticle* Higgs;
-	ElementaryParticle* Top;
-	ElementaryParticle* Z;
 	ElementaryParticle* decayParticle_1;
 	ElementaryParticle* decayParticle_2;
 	
 	Higgs = new ElementaryParticle("Higgs boson", 125.35, 1);
-	Top = new ElementaryParticle("top quark", 172.76, 0);
-	Z = new ElementaryParticle("Z boson", 91.188, 1);
+	
 	decayParticle_1 = new ElementaryParticle();
 	decayParticle_2 = new ElementaryParticle();
 	
-	Higgs -> printInfo();
-	Top -> printInfo();
-	Z -> printInfo();
-	
-	Higgs -> setMomentum(55.0, 66.0, 77.0);
-	Higgs -> bosonDecay(decayParticle_1, decayParticle_2);
+	for(int i = 0; i < 100; i++)
+	{
+		srand(time(NULL));
+		p1 = ((double) rand() / RAND_MAX)*200.0 - 100.0;
+		p2 = ((double) rand() / RAND_MAX)*200.0 - 100.0;
+		p3 = ((double) rand() / RAND_MAX)*200.0 - 100.0;
+		
+		Higgs -> setMomentum(p1, p2, p3);
+		Higgs -> bosonDecay(decayParticle_1, decayParticle_2);
+		
+		file << i+1;
+		for(int j = 0; j < 4; j++)
+			file << "\t" << decayParticle_1 -> momentum[j];
+		for(int j = 0; j < 4; j++)
+			file << "\t" << decayParticle_2 -> momentum[j];
+		file << "\n";
+	}
 
 	delete Higgs;
-	delete Top;
-	delete Z;
 	delete decayParticle_1;
 	delete decayParticle_2;
+	
+	file.close();
 	
 	return 0;
 }
