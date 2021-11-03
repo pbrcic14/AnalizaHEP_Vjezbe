d287e33e33179b7ed14497721f950fd10a266b5e
pbrcic14 <pbrcic@localhost.localdomain>
2021-11-03 12:22:52 +0100

    Added Vjezbe_5 task1

---
+++ Vjezbe_5/analyze.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <TApplication.h>
+#include <Analyzer.h>
+
+using namespace std;
+
+int main()
+{	
+	Analyzer* thing;
+	thing = new Analyzer();
+	
+	thing -> Loop();
+	
+	delete thing;
+	
+	return 0;
+}
