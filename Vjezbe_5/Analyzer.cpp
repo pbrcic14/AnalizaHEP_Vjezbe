b6738dcec79ca25c14382e15ef87557b3d00515e
pbrcic14 <pbrcic@localhost.localdomain>
2021-11-03 13:33:42 +0100

    Added Vjezbe_5 task3

--- Vjezbe_5/Analyzer.cpp
+++ Vjezbe_5/Analyzer.cpp
@@ -2,6 +2,7 @@
 #include "Analyzer.h"
 #include <TH2.h>
 #include <TH1F.h>
+#include <TLegend.h>
 #include <TStyle.h>
 #include <TCanvas.h>
 
@@ -12,7 +13,8 @@ void Analyzer::Loop()
    Long64_t nentries = fChain->GetEntriesFast();
 
    Long64_t nbytes = 0, nb = 0;
-   for (Long64_t jentry=0; jentry<nentries;jentry++) {
+   for (Long64_t jentry=0; jentry<nentries;jentry++)
+   {
       Long64_t ientry = LoadTree(jentry);
       if (ientry < 0) break;
       nb = fChain->GetEntry(jentry);   nbytes += nb;
@@ -21,23 +23,38 @@ void Analyzer::Loop()
 }
 void Analyzer::PlotHistogram()
 {
-	TH1F* histo = new TH1F("histo", "pt", 100, 0, 200);
+	TH1F* histo = new TH1F("histo", "pt", 100, 0, 150);
 	if (fChain == 0) return;
 
-   Long64_t nentries = fChain->GetEntriesFast();
+	Long64_t nentries = fChain->GetEntriesFast();
 
-   Long64_t nbytes = 0, nb = 0;
-   for (Long64_t jentry=0; jentry<nentries;jentry++) {
-      Long64_t ientry = LoadTree(jentry);
-      if (ientry < 0) break;
-      nb = fChain->GetEntry(jentry);   nbytes += nb;
-      // if (Cut(ientry) < 0) continue;
-      histo->Fill(pt1);
-   }
+	Long64_t nbytes = 0, nb = 0;
+	for (Long64_t jentry=0; jentry<nentries;jentry++)
+	{
+		Long64_t ientry = LoadTree(jentry);
+		if (ientry < 0) break;
+		nb = fChain->GetEntry(jentry);   nbytes += nb;
+		// if (Cut(ientry) < 0) continue;
+		histo->Fill(pt1);
+	}
    
-   TCanvas* c = new TCanvas("c", "c", 900, 900);
-   histo->Draw();
-   c->SaveAs("pt1.pdf");
-   c->SaveAs("pt1.png");
-   c->SaveAs("pt1.root");
+	TCanvas* c = new TCanvas("c", "c", 900, 900);
+	
+	histo->SetTitle(TString("Transversal momentum of decay particle 1"));
+	histo->GetXaxis()->SetTitle("pt1 (GeV/c)");
+	histo->GetYaxis()->SetTitle("N");
+	histo->SetLineColor(2);
+	histo->SetFillColor(2);
+	histo->SetStats(0);
+	histo->Draw();
+	
+	TLegend *legend= new TLegend(0.7,0.7,0.9,0.8);
+	
+	legend->SetHeader("Legend","C");
+	legend->AddEntry(histo,"Simulated pt1","l");
+	legend->Draw("pt1");
+	
+	c->SaveAs("pt1.pdf");
+	c->SaveAs("pt1.png");
+	c->SaveAs("pt1.root");
 }
