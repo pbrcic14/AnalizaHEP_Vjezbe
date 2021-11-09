f543fd08d00098f74c0b465c8642f24561cd706c
pbrcic14 <pbrcic@localhost.localdomain>
2021-11-09 14:05:18 +0100

    Add Vjezbe_5 task5

--- Vjezbe_5/Analyzer.cpp
+++ Vjezbe_5/Analyzer.cpp
@@ -5,19 +5,20 @@
 #include <TLegend.h>
 #include <TStyle.h>
 #include <TCanvas.h>
+#include <TLorentzVector.h>
 
 void Analyzer::Loop()
 {
    if (fChain == 0) return;
-
    Long64_t nentries = fChain->GetEntriesFast();
-
    Long64_t nbytes = 0, nb = 0;
    for (Long64_t jentry=0; jentry<nentries;jentry++)
    {
       Long64_t ientry = LoadTree(jentry);
-      if (ientry < 0) break;
-      nb = fChain->GetEntry(jentry);   nbytes += nb;
+      if (ientry < 0)
+      	break;
+      nb = fChain->GetEntry(jentry);
+      nbytes += nb;
       // if (Cut(ientry) < 0) continue;
       
       cout << name1 << endl;
@@ -25,47 +26,67 @@ void Analyzer::Loop()
 }
 void Analyzer::PlotHistogram()
 {
-	TH1F* histo1 = new TH1F("histo1", "pt1", 100, 0, 150);
-	TH1F* histo2 = new TH1F("histo2", "pt2", 100, 0, 150);
+	TCanvas* c = new TCanvas("c", "c", 1800, 900);
+	c->Divide(2);
 	
-	if (fChain == 0) return;
-
+	TH1F* histo1 = new TH1F("histo1", "Transversal momentum of decay particles", 100, 0, 150);
+	TH1F* histo2 = new TH1F("histo2", "Transversal momentum of decay particles", 100, 0, 150);
+	TH1F* histoH = new TH1F("histoH", "Transversal momentum of reconstructed Higgs", 100, 0, 150);
+	
+	TLorentzVector* Higgs = new TLorentzVector();
+	
+	if (fChain == 0)
+		return;
 	Long64_t nentries = fChain->GetEntriesFast();
-
 	Long64_t nbytes = 0, nb = 0;
 	for (Long64_t jentry=0; jentry<nentries;jentry++)
 	{
 		Long64_t ientry = LoadTree(jentry);
-		if (ientry < 0) break;
-		nb = fChain->GetEntry(jentry);   nbytes += nb;
+		if (ientry < 0)
+			break;
+		nb = fChain->GetEntry(jentry);
+		nbytes += nb;
 		// if (Cut(ientry) < 0) continue;
+		
 		histo1->Fill(pt1);
 		histo2->Fill(pt2);
+		
+		Higgs->SetPxPyPzE(px1+px2, py1+py2, pz1+pz2, E1+E2);
+		histoH->Fill(Higgs->Pt());
 	}
-   
-	TCanvas* c = new TCanvas("c", "c", 900, 900);
 	
-	histo1->SetTitle(TString("Transversal momentum of decay particles"));
+	c->cd(1);
 	histo1->GetXaxis()->SetTitle("p_{T} (GeV/c)");
 	histo1->GetYaxis()->SetTitle("no. of events");
-	histo1->SetLineColor(857); // some blue
-	histo1->SetFillColor(857);
+	histo1->SetLineColor(kBlue-4);
+	histo1->SetFillColor(kBlue-4);
 	histo1->SetStats(0);
+	histo1->Draw();
 	
 	histo2->SetLineColor(kRed);
 	histo2->SetLineWidth(3);
-	
-	histo1->Draw();
 	histo2->Draw("hist same");
 	
-	TLegend *legend= new TLegend(0.7,0.9,0.9,0.8);
-	
-	legend->SetHeader("Simulated for:","C");
-	legend->AddEntry(histo1,"particle 1","f");
-	legend->AddEntry(histo2,"particle 2","l");
+	TLegend* legend = new TLegend(0.6,0.8,0.9,0.9);
+	legend->SetTextSize(0.03);
+	legend->AddEntry(histo1,"Decay particle 1","f");
+	legend->AddEntry(histo2,"Decay particle 2","l");
 	legend->Draw();
 	
-	c->SaveAs("pt.pdf");
-	c->SaveAs("pt.png");
-	c->SaveAs("pt.root");
+	c->cd(2);
+	histoH->GetXaxis()->SetTitle("p_{T} (GeV/c)");
+	histoH->GetYaxis()->SetTitle("no. of events");
+	histoH->SetLineColor(kGreen+2);
+	histoH->SetFillColor(kGreen+2);
+	histoH->SetStats(0);
+	histoH->Draw();
+	
+	TLegend* legendH = new TLegend(0.1,0.8,0.45,0.9);
+	legendH->SetTextSize(0.03);
+	legendH->AddEntry(histoH, "Reconstructed Higgs", "f");
+	legendH->Draw();	
+	
+	c->SaveAs("Reconstructed.pdf");
+	c->SaveAs("Reconstructed.png");
+	c->SaveAs("Reconstructed.root");
 }
