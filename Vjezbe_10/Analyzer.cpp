f3c6e10053e8b5a5aeeb0bae77848545057346fd
pbrcic14 <pbrcic@symmetry.pmfst.hr>
2021-12-22 00:53:08 +0100

    Add Vjezbe_10 task2 (comparison with analytical)

--- Vjezbe_10/Analyzer.cpp
+++ Vjezbe_10/Analyzer.cpp
@@ -7,65 +7,44 @@
 #include <TGraphErrors.h>
 #include <iostream>
 #include <TMath.h>
-#include <TLine.h>
 
 using namespace std;
 
 void Analyzer::Drawing()
 {
-	TCanvas* c = new TCanvas("c", "c", 2000, 900);
-	c->Divide(2);
-	gStyle->SetOptFit();
-	
 	const Int_t n = 5;
 	Double_t y[n]  = {9.8, 21.2, 34.5, 39.9, 48.5};
 	Double_t yErr[n]  = {1.0, 1.9, 3.1, 3.9, 5.1};
 	Double_t x[n] = {1., 2., 3., 4., 5.};
 	Double_t xErr[n] = {0., 0., 0., 0., 0.};
 	
-	c->cd(1);						// 
-	
-	TGraphErrors* gr = new TGraphErrors(n,x,y,xErr,yErr);
-	gr->SetTitle("Acceleration vs force; F (N); a (m/s^{2})");
-	gr->SetMarkerColor(4);
-	gr->SetMarkerStyle(21);
-	gr->Draw("AP");
-	
-	TF1 *func;
-	func = new TF1("func", "x/[0]" ,0.0, 5.0);
-	func->SetParName(0, "mass");
-	func->SetParameter(0, 1.0);
-	gr->Fit(func);
-	
-	c->cd(2);						// 
-	
 	TF1 *funcChi2;
-	funcChi2 = new TF1("funcChi2", "pow((9.8-1.0*x)/1.0,2) + pow((21.2-2.0*x)/1.9,2) + pow((34.5-3.0*x)/3.1,2) +  pow((39.9-4.0*x)/3.9,2) + pow((48.5-5.0*x)/5.1,2)",9.5, 11.0);
-	funcChi2->SetTitle("Chi-square; #theta; y");
-	funcChi2->Draw();
+	funcChi2 = new TF1("funcChi2", "pow((9.8-1.0*x)/1.0,2) + pow((21.2-2.0*x)/1.9,2) + pow((34.5-3.0*x)/3.1,2) +  pow((39.9-4.0*x)/3.9,2) + pow((48.5-5.0*x)/5.1,2)",0.0, 20.0);
 	
-	double theta = funcChi2->GetMinimumX();
-    double x1 = funcChi2->GetX(funcChi2->GetMinimum() + 1.0, 1.0, funcChi2->GetMinimumX() - 0.0001);
-    double x2 = funcChi2->GetX(funcChi2->GetMinimum() + 1.0, funcChi2->GetMinimumX() + 0.0001, 20.0);
-	double sig1 = funcChi2->GetMinimumX() - x1;
-	double sig2 = x2 - funcChi2->GetMinimumX();
-	double mass = 1.0 / theta;
-	double massSig1 = sig1 / pow(theta,2);
-	double massSig2 = sig2 / pow(theta,2);
+	// task3
+	double theta, x1, x2, sig1, sig2, mass, massSig1, massSig2;
+	theta = funcChi2->GetMinimumX();
+    x1 = funcChi2->GetX(funcChi2->GetMinimum() + 1.0, 1.0, funcChi2->GetMinimumX() - 0.0001);
+    x2 = funcChi2->GetX(funcChi2->GetMinimum() + 1.0, funcChi2->GetMinimumX() + 0.0001, 20.0);
+	sig1 = funcChi2->GetMinimumX() - x1;
+	sig2 = x2 - funcChi2->GetMinimumX();
+	mass = 1.0 / theta;
+	massSig1 = sig1 / pow(theta,2);
+	massSig2 = sig2 / pow(theta,2);
+	cout << "FROM FUNCTION" << endl;
 	cout << "Minimum at mass = " << mass << " +" << massSig1 << " -" << massSig2 << endl;
 	
-	TLine *l = new TLine(theta, 2.0, theta, funcChi2->GetMinimum());
-	l->SetLineStyle(2);
-	l->Draw();
-	TLine *l1 = new TLine(theta-sig1, 2.0, theta-sig1, funcChi2->GetMinimum()+1);
-	l1->SetLineStyle(2);
-	l1->Draw();
-	TLine *l2 = new TLine(theta+sig2, 2.0, theta+sig2, funcChi2->GetMinimum()+1);
-	l2->SetLineStyle(2);
-	l2->Draw();
-	TLine *lh = new TLine(theta-sig1, funcChi2->GetMinimum()+1, theta+sig2, funcChi2->GetMinimum()+1);
-	lh->SetLineStyle(2);
-	lh->Draw();
-	
-	c->SaveAs("ChiSquare.png");
+	// task2 (analytical)
+	double numerator = 0.0, denominator = 0.0, thetaSig, massSig;
+	for(int i=0; i<n; i++)
+	{
+		numerator += (x[i]*y[i]) / (yErr[i]*yErr[i]);
+		denominator += (x[i]*x[i]) / (yErr[i]*yErr[i]);
+	}
+	theta = numerator / denominator;
+	thetaSig = sqrt(2.0*denominator);
+	mass = 1.0 / theta;
+	massSig = thetaSig / (theta*theta);
+	cout << "ANALYTICAL" << endl;
+	cout << "Minimum at mass = " << mass << " +-" << massSig << endl;
 }
